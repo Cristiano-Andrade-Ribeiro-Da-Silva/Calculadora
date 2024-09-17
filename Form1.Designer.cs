41848ee6b6fa1a373a2c5ebf175f741d208d3b69
Cristiano-Andrade-Ribeiro-Da-Silva <cristiano060608@gmail.com>
2024-09-17 09:40:59 -0300

    Foi colocado uma tela de carregamento apenas na aparêcia, com um gif.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,19 +29,60 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            progressBar1 = new ProgressBar();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(185, 399);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(400, 50);
+            progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 20.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(325, 353);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 31);
+            label1.TabIndex = 1;
+            label1.Text = "Loading...";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Image = Properties.Resources.monkey_14540_256;
+            pictureBox1.Location = new Point(133, 299);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(200, 150);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(784, 461);
+            Controls.Add(label1);
+            Controls.Add(progressBar1);
+            Controls.Add(pictureBox1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             Text = "Calculadora";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ProgressBar progressBar1;
+        private Label label1;
+        private PictureBox pictureBox1;
     }
 }
