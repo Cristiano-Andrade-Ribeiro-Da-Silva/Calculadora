6e7c511b627b45f6e1149f6ee99dcfdb6d0ee388
Cristiano-Andrade-Ribeiro-Da-Silva <cristiano060608@gmail.com>
2024-09-17 10:11:00 -0300

    Modify:fundo decorado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,12 +32,14 @@
             progressBar1 = new ProgressBar();
             label1 = new Label();
             pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // progressBar1
             // 
-            progressBar1.Location = new Point(185, 399);
+            progressBar1.Location = new Point(169, 323);
             progressBar1.Name = "progressBar1";
             progressBar1.Size = new Size(400, 50);
             progressBar1.TabIndex = 0;
@@ -46,37 +48,54 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Arial", 20.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
-            label1.Location = new Point(325, 353);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(309, 268);
             label1.Name = "label1";
             label1.Size = new Size(134, 31);
             label1.TabIndex = 1;
             label1.Text = "Loading...";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // pictureBox1
             // 
             pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
             pictureBox1.Image = Properties.Resources.monkey_14540_256;
-            pictureBox1.Location = new Point(133, 299);
+            pictureBox1.Location = new Point(114, 217);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(200, 150);
+            pictureBox1.Size = new Size(200, 156);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 2;
             pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.pngwing1;
+            pictureBox2.Location = new Point(-2, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(788, 462);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkGreen;
+            BackgroundImageLayout = ImageLayout.Zoom;
             ClientSize = new Size(784, 461);
             Controls.Add(label1);
             Controls.Add(progressBar1);
             Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
+            DoubleBuffered = true;
             FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Calculadora";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -86,5 +105,6 @@
         private ProgressBar progressBar1;
         private Label label1;
         private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,25 @@ namespace Calculadora.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pngwing_com {
+            get {
+                object obj = ResourceManager.GetObject("pngwing.com", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap pngwing1 {
+            get {
+                object obj = ResourceManager.GetObject("pngwing1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
