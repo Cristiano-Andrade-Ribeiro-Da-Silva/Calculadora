07496241266a9d2b2a5cf668d0b32ca78c706e61
Cristiano Andrade Ribeiro da Silva <cristiano060608@gmail.com>
2024-09-24 11:24:50 -0300

    Modify corrigindo o local da calculadora.

---
+++ Form2.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Calculadora
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button15 = new Button();
+            button14 = new Button();
+            button13 = new Button();
+            button12 = new Button();
+            button11 = new Button();
+            button10 = new Button();
+            button9 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            button0 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Arial", 12F, FontStyle.Italic);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(325, 93);
+            label1.TabIndex = 33;
+            label1.Text = "0";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button15
+            // 
+            button15.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button15.Location = new Point(362, 9);
+            button15.Name = "button15";
+            button15.Size = new Size(93, 37);
+            button15.TabIndex = 32;
+            button15.Text = "<_x_]";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button14.Location = new Point(362, 64);
+            button14.Name = "button14";
+            button14.Size = new Size(93, 37);
+            button14.TabIndex = 31;
+            button14.Text = "--:--";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button13.Location = new Point(362, 119);
+            button13.Name = "button13";
+            button13.Size = new Size(93, 37);
+            button13.TabIndex = 30;
+            button13.Text = "x";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button12.Location = new Point(362, 175);
+            button12.Name = "button12";
+            button12.Size = new Size(93, 37);
+            button12.TabIndex = 29;
+            button12.Text = "--";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button11.Location = new Point(362, 232);
+            button11.Name = "button11";
+            button11.Size = new Size(93, 37);
+            button11.TabIndex = 28;
+            button11.Text = "+";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button10.Location = new Point(362, 289);
+            button10.Name = "button10";
+            button10.Size = new Size(93, 37);
+            button10.TabIndex = 27;
+            button10.Text = "=";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button9.Location = new Point(244, 119);
+            button9.Name = "button9";
+            button9.Size = new Size(93, 37);
+            button9.TabIndex = 26;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button8.Location = new Point(129, 119);
+            button8.Name = "button8";
+            button8.Size = new Size(93, 37);
+            button8.TabIndex = 25;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button7.Location = new Point(12, 119);
+            button7.Name = "button7";
+            button7.Size = new Size(93, 37);
+            button7.TabIndex = 24;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button6.Location = new Point(244, 175);
+            button6.Name = "button6";
+            button6.Size = new Size(93, 37);
+            button6.TabIndex = 23;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button5.Location = new Point(129, 175);
+            button5.Name = "button5";
+            button5.Size = new Size(93, 37);
+            button5.TabIndex = 22;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button4.Location = new Point(12, 175);
+            button4.Name = "button4";
+            button4.Size = new Size(93, 37);
+            button4.TabIndex = 21;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button3.Location = new Point(244, 232);
+            button3.Name = "button3";
+            button3.Size = new Size(93, 37);
+            button3.TabIndex = 20;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button2.Location = new Point(129, 232);
+            button2.Name = "button2";
+            button2.Size = new Size(93, 37);
+            button2.TabIndex = 19;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button1.Location = new Point(12, 232);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 37);
+            button1.TabIndex = 18;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            button0.Font = new Font("Arial", 12F, FontStyle.Italic);
+            button0.Location = new Point(129, 289);
+            button0.Name = "button0";
+            button0.Size = new Size(93, 37);
+            button0.TabIndex = 17;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(469, 346);
+            Controls.Add(label1);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(button0);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button15;
+        private Button button14;
+        private Button button13;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button button0;
+    }
+}
